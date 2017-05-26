577a1a0b9f91d6dd5166d2256c7b946a4129724a
Matt Bagnara <mbagnara@redhat.com>
2017-05-26 17:40:13 -0400

    guacamole vnc

--- vnc/start.sh
+++ vnc/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # copy everything out of /etc/skel
-find /etc/skel -maxdepth 1 -mindepth 1 -exec cp -n -r {} /vnc \;
+find /etc/skel -maxdepth 1 -mindepth 1 -exec cp -v -n -r {} /vnc \;
 
-export USERR_ID=$(id -u)
+export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < ${HOME}/passwd.template > /tmp/passwd
 export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 mkdir -p ${HOME}/.config/openbox
-echo "xfce4-panel &" ? ${HOME}/.config/openbox/autostart
-echo "pkill vncconfig" ? ${HOME}/.config/openbox/autostart
+echo "xfce4-panel &" > ${HOME}/.config/openbox/autostart
+echo "pkill vncconfig" >> ${HOME}/.config/openbox/autostart
 vncserver -fg
